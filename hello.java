6da4d91fe8004c76131b0379464fd20028df4cc8
Steve Bento <sbento@mitre.org>
2017-03-09 14:49:07 -0500

    test

--- hello.java
+++ hello.java
@@ -2,7 +2,5 @@ public class hello {
 	public static void main(String[] args)
 	{ //Prints "Hello Java!"
 	System.out.println("Hello Java!");
-	System.out.println("Hello Java! line2 ");
-	System.out.println("Hello Java! line3 ");
 	}
 }
