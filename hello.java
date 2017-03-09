71aa85b3c571b90fc92a858b8266d268693f4349
Steve Bento <sbento@mitre.org>
2017-03-09 14:41:43 -0500

    modified hello.java

--- hello.java
+++ hello.java
@@ -2,5 +2,7 @@ public class hello {
 	public static void main(String[] args)
 	{ //Prints "Hello Java!"
 	System.out.println("Hello Java!");
+	System.out.println("Hello Java! line2 ");
+	System.out.println("Hello Java! line3 ");
 	}
 }
